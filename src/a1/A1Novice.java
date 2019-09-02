625175a2f538d526d4b2e9e51fecf273396e26ab
harri <harri@DESKTOP-4FG1M86>
2019-09-02 01:57:50 -0400

    line prints

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,24 +9,20 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 		 
 		 int customers = scan.nextInt();
-		 //System.out.println(customers);
+		 System.out.println(customers);
 		 for (int i = 0; i < customers; i++) {
 			 String first = scan.next();
 			 char firstInt = first.charAt (0);
 			 String last = scan.next();
 			 double total = 0.0;
 			 int items = scan.nextInt();
-			 //System.out.println(items);
+			 System.out.println(items);
 			 for (int x = 0; x < items; x++) {
 				int quantity;
 				quantity = scan.nextInt();
 				double price;
-				price=scan.nextDouble();
-				//System.out.println(quantity);
-				double cost;
-				cost=quantity*price;
-				//System.out.println(total);
-			    total = total + cost;
+				price = scan.nextDouble();
+			    total += quantity * price;
 			 }
 			 
 			System.out.println(firstInt + ". " + last + ": " + String.format("%.2f", total));

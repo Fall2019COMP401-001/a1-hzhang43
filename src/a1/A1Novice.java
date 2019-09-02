ee65564c398e79fa2a7c5d383c8750683599ac40
harri <harri@DESKTOP-4FG1M86>
2019-09-02 00:40:37 -0400

    changed scan locations

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,23 +7,25 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		
-		 // for (int i = 0; i < scan.nextInt(); i++) {
+		 
+		 int customers = scan.nextInt();
+		 for (int i = 0; i < customers; i++) {
 			 double total = 0.0;
-			// for (int x = 0; x < scan.nextInt(); x++) {
+			 int items = scan.nextInt();
+			for (int x = 0; x < items; x++) {
 				 int quantity = scan.nextInt();
 				 System.out.println(quantity);
 				 double price = scan.nextDouble();
 				 System.out.println(price);
 				 total += quantity * price;
 				 System.out.println(total);
-			//}
+			}
 			String first = scan.next();
 			char firstInt = first.charAt (0);
 			String last = scan.next();
 			String totalString = Double.toString(total);
 			System.out.println(firstInt + ". " + last + ": " + totalString);
-		// }
+		}
 	
 	}
 	

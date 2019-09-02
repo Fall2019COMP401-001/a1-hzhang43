508d6d7e1e3bba236775821013893eafd323c1a0
harri <harri@DESKTOP-4FG1M86>
2019-09-02 00:30:29 -0400

    added prints

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,19 +8,22 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		  for (int i = 0; i < 1; i++) {
+		 // for (int i = 0; i < scan.nextInt(); i++) {
 			 double total = 0.0;
-			 for (int x = 0; x < 1; x++) {
+			// for (int x = 0; x < scan.nextInt(); x++) {
 				 int quantity = scan.nextInt();
+				 System.out.println(quantity);
 				 double price = scan.nextDouble();
+				 System.out.println(price);
 				 total += quantity * price;
-			}
+				 System.out.println(total);
+			//}
 			String first = scan.next();
 			char firstInt = first.charAt (0);
 			String last = scan.next();
 			String totalString = Double.toString(total);
 			System.out.println(firstInt + ". " + last + ": " + totalString);
-		 }
+		// }
 	
 	}
 	

73076499725348192786051271998d043408a107
harri <harri@DESKTOP-4FG1M86>
2019-09-01 23:39:26 -0400

    debug 2

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,16 +6,15 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
-		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		
-		for (int i = 0; i < scan.nextInt(); i++) {
-			int total = 0;
-			for (int x = 0; x < scan.nextInt(); x++) {
-				int quantity = scan.nextInt();
-				double price = scan.nextDouble();
-				total += quantity * price;
-			}
+		 // for (int i = 0; i < scan.nextInt(); i++) {
+			// int total = 0;
+			// for (int x = 0; x < scan.nextInt(); x++) {
+				// int quantity = scan.nextInt();
+				// double price = scan.nextDouble();
+				// total += quantity * price;
+			// }
 			String first = scan.next();
 			char firstInt = first.charAt (0);
 			String last = scan.next();

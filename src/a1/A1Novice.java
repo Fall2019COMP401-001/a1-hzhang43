bc950e1b47bcd98b3778002a4cd68b8fbe7cfea1
harri <harri@DESKTOP-4FG1M86>
2019-09-02 00:43:56 -0400

    added print statements

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,16 +9,18 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 		 
 		 int customers = scan.nextInt();
+		 System.out.println(customers);
 		 for (int i = 0; i < customers; i++) {
 			 double total = 0.0;
 			 int items = scan.nextInt();
-			for (int x = 0; x < items; x++) {
-				 int quantity = scan.nextInt();
-				 System.out.println(quantity);
-				 double price = scan.nextDouble();
-				 System.out.println(price);
-				 total += quantity * price;
-				 System.out.println(total);
+			 System.out.println(items);
+			 for (int x = 0; x < items; x++) {
+				int quantity = scan.nextInt();
+				System.out.println(quantity);
+				double price = scan.nextDouble();
+				System.out.println(price);
+				total += quantity * price;
+				System.out.println(total);
 			}
 			String first = scan.next();
 			char firstInt = first.charAt (0);

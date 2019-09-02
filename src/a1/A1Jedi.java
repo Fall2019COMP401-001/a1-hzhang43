7336c22e89e6db46ebab86c633ef87b02a26f627
harri <harri@DESKTOP-4FG1M86>
2019-09-02 04:30:35 -0400

    test 1

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,33 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
-		
+		int count = scan.nextInt();
+		String[] food = new String[count];
+		double[] price = new double[count];
+		int[] foodCount = new int[count];
+		int[] customerCount = new int[count];
+		for (int i = 0; i < count; i++) {
+			food[i] = scan.next();
+			price[i] = scan.nextDouble();
+		}
+		int customers = scan.nextInt();
+		for (int x = 0; x < customers; x++) {
+			String first = scan.next();
+			String last = scan.next();
+			int items = scan.nextInt();
+			for (int y = 0; y < items; y++) {
+				int num = scan.nextInt();
+				String itemName = scan.next();
+				for (int z = 0; z < count; z++) {
+					if (food[z].equals(itemName)) {
+						foodCount[z] = num;
+						customerCount[z] += 1;
+					}
+				}
+			}
+		}
+		for (int index = 0; index < count; index++) {
+			System.out.println(customerCount[index] + " customers bought " + foodCount[index] + " " + food[index]);
+		}
 	}
 }

d80cd0057577d9891605206b1c1183d9a8cd98cf
harri <harri@DESKTOP-4FG1M86>
2019-09-02 02:02:28 -0400

    removed prints

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,14 +9,12 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 		 
 		 int customers = scan.nextInt();
-		 System.out.println(customers);
 		 for (int i = 0; i < customers; i++) {
 			 String first = scan.next();
 			 char firstInt = first.charAt (0);
 			 String last = scan.next();
 			 double total = 0.0;
 			 int items = scan.nextInt();
-			 System.out.println(items);
 			 for (int x = 0; x < items; x++) {
 				int	quantity = scan.nextInt();
 				String food = scan.next();
